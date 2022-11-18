5bdf8984823b3f1d6d4941946c6760d56dfeab24
Matthew Colter <matthewkastor@gmail.com>
2022-11-17 19:13:51 -0500

    update dev

--- docs/vsdoc/symbols/atropa-vsdoc.js
+++ docs/vsdoc/symbols/atropa-vsdoc.js
@@ -5,10 +5,14 @@
     
 
     window.atropa = {
-        /// <summary></summary>
-        /// <field name="data" type="">Container for gobal data related to the classes and functions.</field>
-        /// <field name="inquire" type="">Container for functions that test the state of inputs.</field>
+        /// <summary>Container for all Glorious classes, functions, etc.</summary>
         /// <returns type="atropa"/>
+      
+        /// <field name="data" type="">Container for gobal data related to the classes and functions.</field>
+        data : {}, 
+      
+        /// <field name="inquire" type="">Container for functions that test the state of inputs.</field>
+        inquire : {}, 
                 
         supportCheck: function(className, errorMessage) {
             /// <summary>Checks whether this class has been marked as unsupported and throws an 
